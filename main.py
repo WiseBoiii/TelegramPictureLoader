4aa5ac9b9913e258894442dc5e4d2593a0ee2a1e
WiseBoiii <64275846+WiseBoiii@users.noreply.github.com>
2021-10-01 18:18:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+load_dotenv()
+API_KEY = os.environ.get('API_KEY')
+nasa_epic_data_url = f'https://api.nasa.gov/EPIC/api/natural/images?api_key={API_KEY}'
+spacex_launch_data_url = 'https://api.spacexdata.com/v3/launches/64'
+nasa_apod_data_url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}&count=50'
+
+
+def load_spacex_pictures(spacex_launch_data_url):
+    response = requests.get(spacex_launch_data_url)
+    response.raise_for_status
+    launch_data = response.json()
+    flickr_links = launch_data['links']['flickr_images'][:]
+    for iteration_index, picture_link in enumerate(flickr_links):
+        response = requests.get(picture_link)
+        response.raise_for_status()
+        spacex_picture_extension = get_picture_extension(picture_link)
+        filename = f'images/spacex_images/spacex_image_{iteration_index}{spacex_picture_extension}'
+        out = open(filename, "wb")
+        out.write(response.content)
+        out.close()
+
+def load_nasa_apod_pictures(nasa_apod_data_url):
+    response = requests.get(nasa_apod_data_url)
+    nasa_apod_data = response.json()
+    response.raise_for_status()
+    for iteration_index, nasa_apod_data in enumerate(nasa_apod_data):
+        nasa_picture_url = nasa_apod_data['url']
+        response = requests.get(nasa_picture_url)
+        response.raise_for_status()
+        picture_extension = get_picture_extension(nasa_picture_url)
+        filename = f'images/nasa_images/nasa_apod_image_{iteration_index}{picture_extension}'
+        out = open(filename, "wb")
+        out.write(response.content)
+        out.close()
+
+def load_nasa_epic_pictures(nasa_epic_data_url):
+    response = requests.get(nasa_epic_data_url)
+    nasa_epic_picture_data = response.json()
+    response.raise_for_status()
+    for iteration_index, nasa_epic_launch_data in enumerate(nasa_epic_picture_data):
+        pprint(nasa_epic_launch_data)
+        launch_date = nasa_epic_launch_data['date'].split(' ')[0].replace('-', '/')
+        image_name = nasa_epic_launch_data['image']
+        nasa_epic_url = f'https://api.nasa.gov/EPIC/archive/natural/{launch_date}/png/{image_name}.png?api_key={API_KEY}'
+        print(nasa_epic_url)
+        response = requests.get(nasa_epic_url)
+        response.raise_for_status()
+        filename = f'images/nasa_epic_images/nasa_epic_image_{iteration_index}.png'
+        out = open(filename, "wb")
+        out.write(response.content)
+        out.close()
+
+
+def get_picture_extension(picture_link):
+    _, extension = os.path.splitext(picture_link)
+    return extension
+
+
+if __name__ == '__main__':
+    '''load_spacex_pictures(spacex_launch_data_url)'''
+    '''load_nasa_apod_pictures(nasa_apod_data_url)'''
+    load_nasa_epic_pictures(nasa_epic_data_url)
