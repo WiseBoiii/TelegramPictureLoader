048da625030af2f68a9da20904c26cf59519e177
WiseBoiii <64275846+WiseBoiii@users.noreply.github.com>
2022-02-06 21:36:47 +0300

    Updated main.py
    - Removed unnessecary try/except
    - Fixed download_apod_pics() name

--- main.py
+++ main.py
@@ -27,12 +27,9 @@ def get_nasa_apod_picture_urls(nasa_api_key, picture_amount):
     response.raise_for_status()
     nasa_apod = response.json()
     urls = []
-    for iteration_index, nasa_apod_info in enumerate(nasa_apod):
-        try:
-            if nasa_apod_info['media_type'] == 'image':
-                urls.append(nasa_apod_info['url'])
-        except KeyError:
-            pass
+    for nasa_apod_info in nasa_apod:
+        if nasa_apod_info['media_type'] == 'image':
+            urls.append(nasa_apod_info['url'])
     return urls
 
 
@@ -60,7 +57,7 @@ def download_picture(picture_link, picture_name, picture_directory, payload=''):
         filename.write(response.content)
 
 
-def download_apod_urls(urls, main_image_directory, nasa_apod_dir_name):
+def download_apod_pics(urls, main_image_directory, nasa_apod_dir_name):
     for index, nasa_apod_url in enumerate(urls):
         apod_picture_name = f'nasa_apod_image_{index}'
         picture_directory = f'{main_image_directory}/{nasa_apod_dir_name}'
@@ -104,6 +101,6 @@ if __name__ == '__main__':
         load_spacex_pictures(main_image_directory, spacex_dir_name, 64)
         apod_urls = get_nasa_apod_picture_urls(nasa_api_key, 50)
         load_nasa_epic_pictures(nasa_api_key, main_image_directory, nasa_epic_dir_name)
-        download_apod_urls(apod_urls, main_image_directory, nasa_apod_dir_name)
+        download_apod_pics(apod_urls, main_image_directory, nasa_apod_dir_name)
         upload_post_to_chat(main_image_directory, telegram_chat_id, space_pic_dirs, bot)
         sleep(post_delay)
